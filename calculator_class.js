c2bdd7aa782051f5ff017e2e7752cdf27521f664
mat09876 <mmcdonnell@cartarwler.com>
2016-03-03 17:26:59 +0000

    more operators

--- calculator_class.js
+++ calculator_class.js
@@ -2,123 +2,273 @@
 	var Cal = function() {
 		this.buttonConf = {
 			rad: {
-				text: 'Rad'
+				text: 'Rad',
+				bg: 'dark',
+				value: 'rad',
+				isOperater: !0,
+				operator: 'rad'
 			},
 			lots_of_dots: {
-				text: 'nothing'
+				text: 'dots',
+				bg: 'light',
+				value: 'dots',
+				isOperater: !1,
+				operator: 'lots_of_dots'
 			},
 			xl: {
-				text: 'xl'
+				text: 'xl',
+				bg: 'dark',
+				value: 'xl',
+				isOperater: !0,
+				operator: 'xl'
 			},
 			bracket_open: {
-				text: '('
+				text: '(',
+				bg: 'dark',
+				value: '(',
+				isOperater : !1,
+				operator: 'bracket_open'
 			},
 			bracket_close: {
-				text: ')'
+				text: ')',
+				bg: 'dark',
+				value: ')',
+				isOperater : !1,
+				operator: 'bracket_close'
 			},
 			percentage: {
-				text: '%'
+				text: '%',
+				bg: 'dark',
+				value: '%',
+				isOperater: !0,
+				operator: 'percentage'
 			},
 			clear: {
-				text: 'AC'
+				text: 'AC',
+				bg: 'dark',
+				value: 'AC',
+				isOperater:!0,
+				operator: 'clear'
+
 			},
 			Inv: {
-				text: 'Inv'
+				text: 'Inv',
+				bg: 'dark',
+				value: 'Inv',
+				isOperater: !0,
+				operator: 'Inv'
 			},
 			sin: {
-				text: 'sin'
+				text: 'sin',
+				bg: 'dark',
+				value: 'sin',
+				isOperater: !0,
+				operator: 'sin'
 			},
 			ln: {
-				text: 'ln'
+				text: 'ln',
+				bg: 'dark',
+				value: 'ln',
+				isOperater: !0,
+				operator: 'ln'
 			},
 			seven: {
-				text: '7'
+				text: '7',
+				bg: 'light',
+				value: '7',
+				isOperater: !1,
+				operator: 'seven'
 			},
 			eight: {
-				text: '8'
+				text: '8',
+				bg: 'light',
+				value: '8',
+				isOperater: !1,
+				operator: 'eight'
+
 			},
 			nine: {
-				text: '9'
+				text: '9',
+				bg: 'light',
+				value: '9',
+				isOperater: !1,
+				operator: 'nine'
 			},
 			divide: {
-				text: '&#xF7'
+				text: '&#xF7',
+				bg: 'dark',
+				value: '/',
+				isOperater: !0,
+				operator: 'divide'
 			},
 			Pi: {
-				text: '&#960'
+				text: '&#960',
+				bg: 'dark',
+				value: 'pi',
+				isOperater: !0,
+				operator: 'pi'
 			},
 			cos: {
-				text: 'cos'
+				text: 'cos',
+				bg: 'dark',
+				value: 'cos',
+				isOperater: !0,
+				operator: 'cos'
 			},
 			log: {
-				text: 'log'
+				text: 'log',
+				bg: 'dark',
+				value: 'log',
+				isOperater: !0,
+				operator: 'log'
+
 			},
 			four: {
-				text: '4'
+				text: '4',
+				bg: 'light',
+				value: '4',
+				isOperater: !1,
+				operator: 'four'
 			},
 			five: {
-				text: '5'
+				text: '5',
+				bg: 'light',
+				value: '5',
+				isOperater: !1,
+				operator: 'five'
 			},
 			six: {
-				text: '6'
+				text: '6',
+				bg: 'light',
+				value: '6',
+				isOperater: !1,
+				operator: 'six'
 			},
 			multiply: {
-				text: 'x'
+				text: 'x',
+				bg: 'dark',
+				value: 'x',
+				isOperater: !0,
+				operator: 'multiply'
 			},
 			Irrational_Number: {
-				text: 'e'
+				text: 'e',
+				bg: 'dark',
+				value: 'e',
+				isOperater: !0,
+				operator: 'Irrational_Number'
 			},
 			tan: {
-				text: 'tan'
+				text: 'tan',
+				bg: 'dark',
+				value: 'tan',
+				isOperater: !0,
+				operator: 'tan'
 			},
 			Square_Root: {
-				text: '√'
+				text: '&#8730;',
+				bg: 'dark',
+				value: 'Square_Root',
+				isOperater: !0,
+				operator: 'Square_Root'
 			},
 			one: {
-				text: '1'
+				text: '1',
+				bg: 'light',
+				value: '1',
+				isOperater: !1,
+				operator: 'one'
 			},
 			two: {
-				text: '2'
+				text: '2',
+				bg: 'light',
+				value: '2',
+				isOperater: !1,
+				operator: 'two'
 			},
 			three: {
-				text: '3'
+				text: '3',
+				bg: 'light',
+				value: '3',
+				isOperater: !1,
+				operator: 'three'
 			},
 			minus: {
-				text: '-'
+				text: '-',
+				bg: 'dark',
+				value: '-',
+				isOperater: !0,
+				operator: 'minus'
 			},
 			rad: {
-				text: 'Rad'
+				text: 'Rad',
+				bg: 'light',
+				value: 'rad',
+				isOperater: !0,
+				operator: 'rad'
 			},
 			Ans: {
-				text: 'Ans'
+				text: 'Ans',
+				bg: 'dark',
+				value: 'Ans',
+				isOperater: !0,
+				operator: 'Ans'
 			},
 			
 			
 			EXP: {
-				text: 'EXP'
+				text: 'EXP',
+				bg: 'dark',
+				value: 'EXP',
+				isOperater: !0,
+				operator: 'EXP'
 			},
 			
 			rad: {
-				text: 'Rad'
+				text: 'Rad',
+				bg: 'dark',
+				value: 'rad',
+				isOperater: !0,
+				operator: 'rad'
 			},
 			
 			xy: {
-				text: 'xy'
+				text: 'xy',
+				bg: 'dark',
+				value: 'xy',
+				isOperater: !0,
+				operator: 'xy'
 			},
 			zero: {
-				text: '0'
+				text: '0',
+				bg: 'light',
+				value: '0',
+				isOperater: !1,
+				operator: 'zero'
 			},
 			
 			
 			dot: {
-				text: '.'
+				text: '.',
+				bg: 'light',
+				value: 'dot',
+				isOperater: !1,
+				operator: '.'
 			},	
 			equals: {
-				text: '='
+				text: '=',
+				bg: 'blue',
+				value: 'equals',
+				isOperater: !0,
+				operator: 'equals'
 			},
 			addition : {
-				text: '+'
+				text: '+',
+				bg: 'dark',
+				value: 'addition',
+				isOperater: !0,
+				operator: 'add'
 			}
-
 
 		}	
 		this.parent = document.querySelector('.matt-cool-cal');
@@ -128,6 +278,18 @@
 		this.init();
 	};
 
+	var InputBox = function() {
+		this.value = '';
+		this.dom = document.createElement('div');
+		this.dom.className = 'matt-cool-cal__display';
+		// this.dom.innerHTML = 'display';
+	}
+
+	InputBox.prototype.addText = function(text) {
+		this.value += text;
+		this.dom.innerHTML = this.value;
+	};
+
 	function input() {
 		document.getElementByClass('input').innerHTML = this.text;
 	}
@@ -135,10 +297,16 @@
 	Cal.prototype.init = function() {
 		this.initDisplayArea();
 		this.initButtons();
+		this.values = [];
+		this.operator;
 		this.settings();
 	};
 
-	Cal.prototype.initDisplayArea = function() {};
+	Cal.prototype.initDisplayArea = function() {
+		var display = new InputBox();
+		this.display = display;
+		this.parent.appendChild(display.dom);
+	};
 
 	Cal.prototype.initButtons = function() {
 		var frag = document.createDocumentFragment();
@@ -158,7 +326,9 @@
 				btnWrap = document.createElement('div');
 				btnWrap.className = 'btn-wrap';
 				temp = this.buttonConf[i];
+				//console.log(temp);
 				btn = new CalBtn(temp);
+				btn.cal = this;
 				btnWrap.appendChild(btn.dom);
 				btnsWrap.appendChild(btnWrap);
 			}
@@ -166,29 +336,76 @@
 
 		}
 		// this.parent.appendChild(frag);
-		console.log(this.parent);
+		//console.log(this.parent);
 	};
 
 	Cal.prototype.settings = function() {};
 
+	Cal.prototype.add = function(x, y) {
+		var sum = x + y;
+		this.display.dom.innerHTML = sum;
+		return sum;
+	};
+
+		Cal.prototype.multiply = function(x, y) {
+		var sum = x * y;
+		this.display.dom.innerHTML = sum;
+		return sum;
+	};
+
+		Cal.prototype.divide = function(x, y) {
+		var sum = x / y;
+		this.display.dom.innerHTML = sum;
+		return sum;
+	};
+
+
+
+	Cal.prototype.minus = function(x, y) {
+		var r = x - y;
+		this.display.dom.innerHTML = r;
+		return r;
+	};
+
+	Cal.prototype.doMath = function() {
+		this[this.operator].apply(this, this.values);
+	};
 
 	// Calculator Button Class
 	var CalBtn = function(conf) {
 		this.text = conf.text || 'n/a';
 		this.color = null;
+		this.bg = conf.bg || '#ccc';
 		this.dom = null;
+		this.value = conf.value || 'n/a',
+		this.cal = null;
+		this.isOperater = conf.isOperater;
+		this.operator = this.isOperater ? conf.operator : null;
 		this.init();
 	};
 
 	CalBtn.prototype.init = function() {
+		//console.log(this.conf);
+		var baseCls = 'matt-cool-cal__cal__btn';
 		this.dom = document.createElement('a');
 		this.dom.onclick = this._onClickMe.bind(this);
 		this.dom.innerHTML = this.text;
-		this.dom.className = 'matt-cool-cal__cal__btn';
+		this.dom.className = baseCls + ' ' + baseCls + '--' + this.bg;
 	};
 
 	CalBtn.prototype._onClickMe = function() {
-		console.log(this.text);
+		if(this.text === '=') {
+			this.cal.doMath();
+			return;
+		}
+
+		this.cal.display.addText(this.text);
+		if(this.isOperater) {
+			this.cal.operator = this.operator;
+		} else {
+			this.cal.values.push(parseFloat(this.text));
+		}
+		
 	};
 
 	var myCal = new Cal();
